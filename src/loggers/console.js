8ff209e4d1b6b308173c3a8cc098b29d8ea04c1a
Andrew Plummer <plummer.andrew@gmail.com>
2021-09-19 01:25:58 +0900

    Set default console log level to "warn"

--- src/loggers/console.js
+++ src/loggers/console.js
@@ -4,7 +4,7 @@ const levels = ["trace", "debug", "info", "warn", "error", "fatal"];
 
 class ConsoleLogger {
   _version = "console";
-  _level = process.env.LOG_LEVEL || "trace";
+  _level = process.env.LOG_LEVEL || "warn";
 
   constructor(props = {}) {
     this.props = props;
